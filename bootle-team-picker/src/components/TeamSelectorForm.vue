67d89427297d1a94105fa99378cb6ebc901cae2b
Liam Moorcroft <liammoorcroft@yahoo.co.uk>
2018-04-22 12:20:31 +0100

    - add new form component - new logo added - stripped out scafold content

---
+++ bootle-team-picker/src/components/TeamSelectorForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container is-widescreen">
+        <input v-for="n in 11" type="text" class="input field" :key="n" v-model="firstTeam[n-1]" placeholder="Enter player name"  />
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TeamSelectorForm',
+  data () {
+    return {
+      firstTeam: []
+    }
+  }
+}
+</script>
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/bootlefclogo.png">
     <router-view/>
   </div>
 </template>
