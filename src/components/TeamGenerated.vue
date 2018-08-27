ad3bd00b283451c1f2ead458120ae1aee61774ce
Liam Moorcroft <liammoorcroft@yahoo.co.uk>
2018-08-27 18:17:38 +0100

    outputting players

--- src/components/TeamGenerated.vue
+++ src/components/TeamGenerated.vue
@@ -1,9 +1,29 @@
 <template>
-  <h1>Team Generated Sheet</h1>
+  <div>
+    <h1>Team Generated Sheet</h1>
+    <h2>Starting XI</h2>
+    <p :key="player" v-for="player in startingPlayers">
+      {{ player }}
+    </p>
+
+    <h2>Substitutions</h2>
+    <p :key="sub" v-for="sub in substitutes">
+      {{ sub }}
+    </p>
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: 'TeamGenerated'
+  name: 'TeamGenerated',
+  computed: {
+    startingPlayers () {
+      return this.$store.state.startingPlayers;
+    },
+    substitutes () {
+      return this.$store.state.substitutions;
+    }
+  }
 }
 </script>
